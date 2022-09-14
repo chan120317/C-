82bff6f9b158e07cda53f59eff1fc751138fa01a
chan120317 <chan120317@outlook.kr>
2022-09-15 00:20:47 +0900

    Conditional Statements

---
+++ ConditionalStatements.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+#define FIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+using namespace std;
+
+/*
+* Platform : HackerRank
+* Title : Conditional Statements
+* Rank : Basic
+*/
+
+void staircase(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = n - i; j > 0; j--) cout << ' ';
+        for (int j = 0; j < i; j++) cout << '#';
+        cout << '\n';
+    }
+}
+
+int main() {
+    FIO;
+    int n;
+    cin >> n;
+    staircase(n);
+}
