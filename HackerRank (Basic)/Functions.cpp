e9b8739b6d99f479512b2a522061f170f73dc85f
chan120317 <chan120317@outlook.kr>
2022-09-16 00:22:52 +0900

    Add Functions.cpp

---
+++ HackerRank (Basic)/Functions.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+#define FIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+using namespace std;
+
+/*
+* Platform : HackerRank
+* Title : Functions
+* Rank : Basic
+*/
+
+int max_of_four(int a, int b, int c, int d) {
+    int ret = -1e9;
+    ret = ret > a ? ret : a;
+    ret = ret > b ? ret : b;
+    ret = ret > c ? ret : c;
+    ret = ret > d ? ret : d;
+    return ret;
+}
+
+int main() {
+    FIO;
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int ans = max_of_four(a, b, c, d);
+    cout << ans;
+
+    return 0;
+}
