5048a2e4cedd9e79a34e150c5adb414ad7ebe29e
chan120317 <chan120317@outlook.kr>
2022-09-16 00:23:44 +0900

    Add ForLoop.cpp

---
+++ HackerRank (Basic)/ForLoop.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <string>
+#define endl '\n'
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a, b;
+    cin >> a >> b;
+
+    string numStr[] = { "zero","one","two","three","four","five","six","seven","eight","nine" };
+    for (int i = a; i <= b; i++) {
+        if (1 <= i && i <= 9) cout << numStr[i] << endl;
+        else if (i % 2) cout << "odd" << endl;
+        else cout << "even" << endl;
+    }
+    return 0;
+}
